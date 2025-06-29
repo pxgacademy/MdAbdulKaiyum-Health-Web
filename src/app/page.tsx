125adbd6f3e41a94d1c397e5caa22702d918b258
Abdullah <graphicdesigner.abdullah@gmail.com>
2025-06-29 18:02:02 +0600

    updated BookAppointment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import AboutUs from "@/sections/home/AboutUs";
 import AppointmentAd from "@/sections/home/AppointmentAd";
+import BookAppointment from "@/sections/home/BookAppointment";
 import HappyHistory from "@/sections/home/happyHistory/HappyHistory";
 import HealthyLifestyle from "@/sections/home/healthyLifestyle/HealthyLifestyle";
 import OurServices from "@/sections/home/ourServices/OurServices";
@@ -18,6 +19,7 @@ export default function Home() {
       <VideoSection />
       <HappyHistory />
       <AboutUs />
+      <BookAppointment />
 
       <div className="py-40" />
     </main>
